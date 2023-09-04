e11f0eb441df682287baa3ee585dd3918aef0a94
David Garcia <coderdavidgarcia@gmail.com>
2023-09-04 13:21:54 -0500

    working main.py optimize loop

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
         for risk in risk_values:
             # Your program's main logic here
             data = read_data()
+            
             print(f"read_data(), Reward = {reward}, Risk = {risk}")
             data_with_features = add_features(data, reward, risk)
             print(f"add_features(), Reward = {reward}, Risk = {risk}")
@@ -32,8 +33,10 @@ def main():
             print(f"train_model(), Reward = {reward}, Risk = {risk}")
             predictions, prediction_probabilities = evaluate_model(model, X_train, X_test, y_train, y_test)
             print(f"evaluate_model(), Reward = {reward}, Risk = {risk}")
-            plot_results(data_with_features, predictions, prediction_probabilities, X_test_index)
+            results = plot_results(data_with_features, predictions, prediction_probabilities, X_test_index, reward, risk, results)
             print(f"plot_results(), Reward = {reward}, Risk = {risk}")
+            print(results)
+
 
 
 if __name__ == "__main__":
--- modules/.ipynb_checkpoints/visualization-checkpoint.py
+++ modules/.ipynb_checkpoints/visualization-checkpoint.py
@@ -3,9 +3,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 
-def plot_results(signals_df, testing_signal_predictions, testing_probability_estimates, X_test_index):
-    # Create a predictions DataFrame
-    
+def plot_results(signals_df, testing_signal_predictions, testing_probability_estimates, X_test_index, reward, risk, results):
+
+    # Create a predictions DataFrame for SVM
     predictions_df = pd.DataFrame(index=X_test_index)
     
     predictions_df["predicted_signal"] = testing_signal_predictions
@@ -13,6 +13,7 @@ def plot_results(signals_df, testing_signal_predictions, testing_probability_est
     predictions_df["actual_returns"] = signals_df["close"].pct_change()
     
     predictions_df["algo_returns"] = (signals_df["Exit Price"] - signals_df["close"])/signals_df["close"]
+    
     
     predictions_df["trading_algorithm_returns"] = (
         predictions_df["algo_returns"] * predictions_df["predicted_signal"]
@@ -25,19 +26,88 @@ def plot_results(signals_df, testing_signal_predictions, testing_probability_est
     # Filter rows where the predicted signal is 1 and the probability estimate is >= 0.60
     buy_signals_df = predictions_df[(predictions_df["predicted_signal"] == 1) & (predictions_df["probability_estimates"] >= 0.50)]
     
-    # Calculate cumulative returns for the algorithmic trading strategy
+    # Calculate cumulative returns for the algorithmic trading strategy a
     buy_signals_df["Cumulative Algo Returns"] = (1 + buy_signals_df["trading_algorithm_returns"]).cumprod()
     
-    # Calculate cumulative returns for the actual stock returns
-    predictions_df["Cumulative Actual Returns"] = (1 + predictions_df["actual_returns"]).cumprod()
+    buy_signals_df["Cumulative Actual Returns"] = (1 + predictions_df["actual_returns"]).cumprod()
     
-    # Create the plot
-    plt.figure(figsize=(12, 6))
-    plt.plot(buy_signals_df.index, buy_signals_df["Cumulative Algo Returns"], label="Algorithm Returns (Buy Signals)")
-    plt.plot(predictions_df.index, predictions_df["Cumulative Actual Returns"], label="Actual Returns")
-    plt.xlabel("Date") 
-    plt.ylabel("Cumulative Returns")
-    plt.title("Cumulative Returns of Algorithm vs. Actual Returns")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    ###################################################################################################################
+    ###################################################################################################################
+    ###################################################################################################################
+    
+    # Calculate the time period for which you want to calculate the frequency (e.g., in days)
+    start_date = buy_signals_df.index.min()
+    end_date = buy_signals_df.index.max()
+    time_period_days = (end_date - start_date).days + 1  # Adding 1 to include both start and end dates
+    
+    # Filter the winning, losing, and total trades
+    winning_trades = buy_signals_df[buy_signals_df['trading_algorithm_returns'] > 0]
+    losing_trades = buy_signals_df[buy_signals_df['trading_algorithm_returns'] < 0]
+    total_trades = buy_signals_df["trading_algorithm_returns"]
+    
+    # Calculate gross profit (positive returns) and gross loss (negative returns)
+    gross_profit = buy_signals_df[buy_signals_df['trading_algorithm_returns'] > 0]['trading_algorithm_returns'].sum() * 100
+    gross_loss = buy_signals_df[buy_signals_df['trading_algorithm_returns'] < 0]['trading_algorithm_returns'].sum() * 100
+    
+    # Calculate the average return of your trading strategy
+    average_return = buy_signals_df['trading_algorithm_returns'].mean()
+    
+    # Calculate the risk-free rate (e.g., Treasury bill rate)
+    # You need to specify an appropriate risk-free rate for your analysis
+    risk_free_rate = 0.02  # Replace with the risk-free rate you want to use (e.g., 2% for a Treasury bill)
+    
+    # Calculate downside returns (negative returns)
+    downside_returns = buy_signals_df['trading_algorithm_returns'][buy_signals_df['trading_algorithm_returns'] < 0]
+    
+    # Calculate the downside deviation (standard deviation of negative returns)
+    downside_deviation = (downside_returns * 100).std()
+    
+    # Calculate the Risk:Reward Ratio
+    risk_reward_ratio= reward/risk
+    
+    # Calculate the win rate (if you haven't already)
+    win_rate = len(winning_trades) / len(total_trades)
+    
+    # Calculate the profit factor
+    profit_factor = abs(gross_profit / gross_loss)
+    
+    # Calculate the Sortino Ratio
+    sortino_ratio = (average_return - risk_free_rate) / downside_deviation
+    
+    # Calculate the average profit per winning trade
+    average_profit_per_winning_trade = winning_trades['trading_algorithm_returns'].mean()
+    
+    # Calculate the average loss per losing trade
+    average_loss_per_losing_trade = losing_trades['trading_algorithm_returns'].mean()
+    
+    # Calculate the trade frequency (trades per day)
+    trade_frequency_per_day = len(total_trades) / time_period_days
+    
+    # Calculate the volatility of your trading strategy's returns
+    algo_volatility = buy_signals_df['trading_algorithm_returns'].std()
+    
+    # Calculate cumulative returns for the algorithmic trading strategy
+    cumulative_returns = (1 + buy_signals_df["trading_algorithm_returns"]).cumprod()[-1]
+    
+    
+    # Print the sorted metrics by importance
+    print("Risk:Reward Ratio:", risk_reward_ratio)
+    print("Win Rate (%):", win_rate * 100)
+    print("Profit Factor:", profit_factor)
+    print("Sortino Ratio:", sortino_ratio)
+    print("Average Profit per Winning Trade (%):", average_profit_per_winning_trade * 100)
+    print("Average Loss per Losing Trade (%):", average_loss_per_losing_trade * 100)
+    print("Average Return (%):", average_return * 100)
+    print("Trade Frequency (Trades per Day):", trade_frequency_per_day)
+    print("Downside Deviation:", downside_deviation)
+    print("Volatility (Standard Deviation of Algo Returns):", algo_volatility)
+    print("Gross Profit (%):", gross_profit)
+    print("Gross Loss (%):", gross_loss)
+    print("Total Number of Winning Trades:", len(winning_trades))
+    print("Total Number of Trades:", len(total_trades))
+    print("Risk-Free Rate (%):", risk_free_rate * 100)
+
+    results.append({"Risk": risk, "Reward": reward, "Cumulative Returns": cumulative_returns, "Profit Factor": profit_factor})
+
+    return results
+        
